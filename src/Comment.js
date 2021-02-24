0e8774bce34f11fc2697ed1196f1369ce736ff2f
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-24 14:16:55 -0500

    commit tree b77c31c84ccfaae4ee8e09e91c2babbf9c8bbfc2

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,24 @@
 //your code here
+import React, { Component } from 'react'
+
+class Comment extends React.Component {
+
+    render(){
+        const commentsArray = [
+
+        ]
+        return (
+
+        <div id="comment" className="comment" >
+        {this.props.commentText}
+        </div>
+        )
+
+    }
+   
+}
+
+
+
+
+export default Comment 
